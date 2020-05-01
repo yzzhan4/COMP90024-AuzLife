46283ce36cc458b30c37fca9f8a93e1634c8b3f0
hedgehog7453 <lijy.lijy@gmail.com>
2020-05-01 23:00:48 +1000

    rotate api

---
+++ ThreadPoolManager.py
@@ -0,0 +1,71 @@
+from datetime import datetime, date, time, timedelta
+import tweepy
+import couchdb
+import http
+import threading
+import json
+import time
+
+# Couchdb
+couch = couchdb.Server("http://admin:yosoro@127.0.0.1:5984")
+couch.resource.credentials = ("admin", "yosoro")
+if "test/testdb" not in couch:
+    couch.create("test/testdb")
+db = couch["test/testdb"]
+
+class ThreadPoolManager():
+    def __init__(self, apis):
+        self.lock = threading.Lock()
+        self.apis = apis
+        self.users = []
+        self.threads = []
+        for api in apis:
+            myThread = MyThread(self, api)
+            myThread.start()
+            self.threads.append(myThread)
+    def add_job(self, user_id):
+        print("TM add  " + user_id)
+        self.users.append(user_id)
+    def next_user(self):
+        with self.lock:
+            if len(self.users) != 0:
+                user = self.users[0]
+                self.users.remove(user)
+                return user
+            else:
+                return None
+
+class MyThread(threading.Thread):
+    def __init__(self, tm, api):
+        threading.Thread.__init__(self)
+        self.tm = tm
+        self.api = api
+    def find_related_tweets(self, user_id):
+        try:
+            print(self.name + " looking for related tweets for " + user_id)
+            count = 0
+            for tweet in tweepy.Cursor(self.api.user_timeline, id=user_id).items():
+                create_date = tweet.created_at
+                age_days = (datetime.utcnow() - create_date).days
+                if age_days > 7 :
+                    break
+                if tweet.lang != "en":
+                    continue
+                # hashtag = ""
+                # if hashtag not in tweet.text:
+                #     continue
+                # print(f"{tweet.user.name}:{tweet.text}")
+                # print(age_days)
+                # db.save(json.loads(json.dumps(tweet._json)))
+                count += 1
+            print(self.name + " " + user_id + " " + str(count))
+        except http.client.IncompleteRead:
+            print('=============================error')
+            exit()
+    def run(self):
+        print ("start threading：" + self.name)
+        while(True):
+            next_user = self.tm.next_user()
+            if next_user != None:
+                self.find_related_tweets(next_user)
+        print ("exit threading：" + self.name)
--- streaming.py
+++ streaming.py
@@ -1,111 +1,66 @@
 from __future__ import absolute_import, print_function
 
-from datetime import datetime, date, time, timedelta
-from api_accounts import API_ACCOUNTS
-
 import tweepy
-import couchdb
-import json
-import threading
 import http
+import json
 
-couch = couchdb.Server("http://admin:yosoro@127.0.0.1:5984")
-couch.resource.credentials = ("admin", "yosoro")
-# Create db
-if "test/testdb" not in couch:
-    couch.create("test/testdb")
-db = couch["test/testdb"]
+from api_accounts import API_ACCOUNTS
+from ThreadPoolManager import ThreadPoolManager
 
-def start_streaming(account_name):
-    consumer_key = API_ACCOUNTS[account_name][0]
-    consumer_secret = API_ACCOUNTS[account_name][1]
-    access_token = API_ACCOUNTS[account_name][2]
-    access_token_secret = API_ACCOUNTS[account_name][3]
-    # Authentication
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    # Create API
-    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-    #api = tweepy.API(auth)
+def authenticate():
+    if len(API_ACCOUNTS) < 2:
+        print("Need more accounts")
+        exit()
+    auths = []
+    apis = []
+    for key in API_ACCOUNTS:
+        consumer_key = key[0]
+        consumer_secret = key[1]
+        access_token = key[2]
+        access_token_secret = key[3]
+        # Authentication
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        auths.append(auth)
+        apis.append(tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True))
+    return (auths, apis)
+
+def start_streaming():
+    (auths, apis) = authenticate()
     # Streaming
-    l = StdOutListener(api)
-    stream = tweepy.Stream(auth, l)
+    thread_manager = ThreadPoolManager(apis[1:])
+    l = StdOutListener(apis[0], thread_manager)
+    stream = tweepy.Stream(auths[0], l)
+    stream.filter(locations = [114,-43,152,-10])
     #stream.filter(track=['covid19','Covid19','COVID19','coronavirus','COVID-19','covid 19','COVID 19','Covid 19'])
     #stream.filter(languages = ['en'])
-    stream.filter(locations = [114,-43,152,-10])
     #stream.filter(locations = [-180,-90,180,90])
-
-def find_related_tweets(user_id, api):
-    try:
-        for tweet in tweepy.Cursor(api.user_timeline, id = user_id).items():
-            create_date = tweet.created_at
-            age_days = (datetime.utcnow() - create_date).days
-            if age_days > 7 :
-                break
-            if tweet.lang != "en":
-                continue
-            # hashtag = ""
-            # if hashtag not in tweet.text:
-            #     continue
-            print(f"{tweet.user.name}:{tweet.text}")
-            print(age_days)
-            db.save(json.loads(json.dumps(tweet._json)))
-    except http.client.IncompleteRead:
-        print('=============================error')
-        exit()
-        
-
-class myThread (threading.Thread):
-    def __init__(self, user_id, api):
-        threading.Thread.__init__(self)
-        self.user_id = user_id
-        self.api = api
-    def run(self):
-        print ("start threading：" + self.name)
-        find_related_tweets(self.user_id, self.api)
-        print ("exit threading：" + self.name)
-
-class StdOutListener(tweepy.StreamListener):
-    def __init__(self, api):
-        self.api = api
-        self.me = api.me()
-    def on_data(self, data):
-        tweet = json.loads(data)
-        user_id = ""
-        country_code = "" 
-        country = ""
-        try:
-            user_id = tweet["user"]["screen_name"]
-            country_code = tweet["place"]["country_code"]
-            country = tweet["place"]["country"]
-        except:
-            return True
-        print(user_id + ":" + country_code)
-        # if country == "Australia":
-        #     print(user_id + ":" + country_code)
-        thread1 = myThread(user_id, self.api)
-        thread1.start()
-        return True
-    def on_error(self, status):
-        print(status)
-
-    def on_limit(self,track):
-        print("======================================limit")
-
 
 #TODO: retweets
 
-if __name__ == '__main__':
-    # # Authentication
-    # auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    # auth.set_access_token(access_token, access_token_secret)
+class StdOutListener(tweepy.StreamListener):
+    def __init__(self, api, thread_manager):
+        self.api = api
+        self.me = api.me()
+        self.tm = thread_manager
+    def on_data(self, data):
+        tweet = json.loads(data)
+        user_id = ""
+        # country_code = "" 
+        # country = ""
+        try:
+            user_id = tweet["user"]["screen_name"]
+            # country_code = tweet["place"]["country_code"]
+            # country = tweet["place"]["country"]
+        except:
+            return True
+        # print(user_id + ":" + country_code)
+        self.tm.add_job(user_id)
+        return True
+    def on_error(self, status):
+        print(status)
+    def on_limit(self,track):
+        print("======================================limit")
 
-    # # Create API
-    # api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-    
-    # l = StdOutListener(api)
-    # stream = tweepy.Stream(auth, l)
-    # #stream.filter(track=['covid19','Covid19','COVID19','coronavirus','COVID-19','covid 19','COVID 19','Covid 19'])
-    # #stream.filter(track=["a"])
-    # stream.filter(locations = [114,-43,152,-10])
-    start_streaming('1')
+if __name__ == '__main__':
+    start_streaming()
